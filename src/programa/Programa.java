43e17d788f298bee996ebfc0d14724f17efda665
Walber Martins <walber.silvama@gmail.com>
2019-05-29 16:46:22 -0300

    atualizado menu

--- src/programa/Programa.java
+++ src/programa/Programa.java
@@ -2,15 +2,21 @@ package programa;
 
 import java.util.Scanner;
 
+import listaEncadeada.ListaEncadeada;
+
 public class Programa {
 	
 	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		
+		ListaEncadeada<Integer> lista = new ListaEncadeada<>();
 		int opcao;
+		
 		do {
 			menu();
 			opcao = sc.nextInt();
+			processaOpcao(opcao, lista);
 		}while(opcao != 0);
 	}
 	
@@ -32,17 +38,42 @@ public class Programa {
 		
 	}
 	
-	private static void processaOpcao(int opcao) {
+	private static void processaOpcao(int opcao, ListaEncadeada<Integer> lista) {
+		Integer numero;
 		switch(opcao) {
 		case 1:
+			try {
+				System.out.println();
+				System.out.print("Digite um número inteiro: ");
+				numero = sc.nextInt();
+				lista.addFirst(numero);
+				System.out.printf("O número %d foi adicionado com sucesso.%n", numero);
+			} 
+			catch (Exception e) {
+				e.printStackTrace();
+			}
 			break;
 		case 2:
+			System.out.println();
+			System.out.print("Digite o número a ser excluido: ");
+			numero = sc.nextInt();
+			if(!(lista.remove(numero))) {
+				System.out.printf("O número %d não está na lista.%n", numero);
+			}
+			System.out.printf("O número %d foi excluido com sucesso.%n", numero);
 			break;
 		case 3:
 			menuConsultas();
 			opcao = sc.nextInt();
 			switch(opcao) {
 			case 1:
+				System.out.println();
+				System.out.print("Digite o número a ser consultado: ");
+				numero = sc.nextInt();
+				if(lista.indexOf(numero) != -1)
+					System.out.printf("O número %d está na lista.%n", numero);
+				else
+					System.out.printf("O número %d não está na lista.%n", numero);
 				break;
 			case 2: 
 				break;
@@ -50,8 +81,12 @@ public class Programa {
 				
 			}
 			break;
-		
+		case 0:
+			System.out.println("PROGRAMA ENCERRADO!!!");
+			break;
+		default:
+			System.out.println("Opção inválida. Tente Novamente:");
 		}
 	}
-
+	
 }
